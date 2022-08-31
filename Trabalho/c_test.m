c8bbbdcd88e7e88a48a6a94e8a0bab561f0fb39f
Nogueira <pmsn.mig2002@gmail.com>
2022-08-31 18:01:09 +0100

    update C for only test without training

--- Trabalho/c_test.m
+++ Trabalho/c_test.m
@@ -2,8 +2,8 @@ clc;
 clear;
 close all;
 
-load('Trabalho\Redes\C_RedeTreino' , 'net');
-caminho = dir('Trabalho\Imagens\start\**\*.png');
+load('Trabalho\Out\Redes\C_Test' , 'net');
+caminho = dir('Trabalho\Imagens\test\**\*.png');
 ficheiroCaminho = string({caminho.folder}) + '/' + string({caminho.name});
 str = ficheiroCaminho;
 i = 1;
@@ -44,12 +44,12 @@ for st = str
 
 end
 
-linha1 = repelem(1 , 5);
-linha2 = repelem(2 , 5);
-linha3 = repelem(3 , 5);
-linha4 = repelem(4 , 5);
-linha5 = repelem(5 , 5);
-linha6 = repelem(6 , 5);
+linha1 = repelem(1 , 10);
+linha2 = repelem(2 , 10);
+linha3 = repelem(3 , 10);
+linha4 = repelem(4 , 10);
+linha5 = repelem(5 , 10);
+linha6 = repelem(6 , 10);
 targetMatrix = [linha1 , linha2 , linha3 , linha4 , linha5 , linha6];
 
 targetMatrix = onehotencode(targetMatrix , 1 , 'ClassNames' , 1:6);  %especificar as classes para serem codificadas obter dados logicos
@@ -58,8 +58,8 @@ out = sim(net , matrizBinaria);
 
 r = 0;
 for i = 1 : size(out , 2)
-    [~ , b] = max(out(:,i));
-    [~ , d] = max(targetMatrix(: , i));
+    [a , b] = max(out(:,i));
+    [c , d] = max(targetMatrix(: , i));
     if b == d
         r = r + 1;
     end
@@ -71,6 +71,6 @@ accuracy = (r / size(out , 2) ) * 100;
 fprintf('Precisa total = %f\n' , accuracy);
 
 %Guardar Rede
-rede = "Trabalho\Redes\C_RedeTreino" + ".mat";
+rede = "Trabalho\Out\Redes\C\C_RedeTreino" + ".mat";
 save(rede , 'net');
 
